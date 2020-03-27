ab5a6b91e0fda383055cbabe1565d80085149233
Marilyn <marilyn.gutierrezc@outlook.com>
2020-03-27 12:35:45 +0100

    Buscador lat y lon

--- Letmepark/letmepark/buscador/views.py
+++ Letmepark/letmepark/buscador/views.py
@@ -11,6 +11,35 @@ class Inicio(TemplateView):
     template_name= 'index.html'
 
     
+""" def buscar(request):
+    
+    if request.GET["buscar"]:
+        #mensaje="Articulo buscado: %r" %request.GET["prd"]
+        direccion= request.GET["buscar"]
+        busqueda= EjemploDato.objects.filter(nombre__icontains=direccion) #icontains es como el like de sql asi: like nombre ="raqueta"
+        return render(request,"resultadosbusqueda.html",{"busqueda": busqueda, "query":direccion})
+    else:
+        mensaje="No has introducido nada"
+
+    return HttpResponse(mensaje) """
+
+
+# def buscar(request):
+#     queryset = request.GET.get("buscar")
+#     #EjemploDato = EjemploDato.objects.filter(estado = True)
+#     if queryset:
+#         dato = EjemploDato.objects.filter(
+#             Q(nombre__icontains = queryset) |
+#             Q(direccion__icontains = queryset)
+#         )
+#         return render(request, 'resultadosbusqueda.html',{'dato':dato})
+#     else: 
+#         mensaje ="No encontrado"
+#     return HttpResponse(mensaje)
+
+
+
+
 def buscar(request):
     
     if request.GET["campobuscar"]: 
@@ -19,23 +48,14 @@ def buscar(request):
         #querys = (Q(direccion__nombre__icontains=q) | Q(longitud__latitud__icontains=q))
         #querys = Q(nombre__icontains=q)               
         #ejemplodato = EjemploDato.objects.filter(querys)
-        ejemplodato = EjemploDato.objects.filter(direccion__icontains=q)
+        ejemplodato = EjemploDato.objects.filter(
+            Q(nombre__icontains = q) |
+            Q(direccion__icontains = q)|
+            Q(latitud__icontains = q)|
+            Q(longitud__icontains = q)
+        ).distinct()
         return render(request, "resultadosbusqueda.html", {'busqueda': ejemplodato})
     else:
         mensaje="No has introducido nada"
 
-    return HttpResponse(mensaje)    
-    
-
-''' def buscar(request):
-    
-    if request.GET["campobuscar"]:
-        
-        direccion= request.GET["campobuscar"]
-        busqueda= EjemploDato.objects.filter(nombre__icontains=direccion) 
-        return render(request,"resultadosbusqueda.html",{"busqueda": busqueda, "query":direccion})
-    else:
-        mensaje="No has introducido nada"
-
-    return HttpResponse(mensaje) '''
-
+    return HttpResponse(mensaje) 
