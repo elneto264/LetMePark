abf8b61d26cad4169ef9f3d08c077843b10c10bc
Ernesto Silva Languasco <ernestoster@gmail.com>
2020-04-02 22:13:48 +0200

    req9

--- Letmepark/letmepark/letmepark/settings.py
+++ Letmepark/letmepark/letmepark/settings.py
@@ -152,11 +152,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS=(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
