87e44073212a9e80b48cbb5a323faa28ba7d724c
Steven de Salas <sdesalas@users.noreply.github.com>
2020-04-15 00:45:07 +0200

    Update settings.py

--- Letmepark/letmepark/letmepark/settings.py
+++ Letmepark/letmepark/letmepark/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
          },
         'NAME': 'letmepark',
         'CLIENT': {
-            'host': 'mongodb+srv://students:a428PmHV6DzfPMnk@mongodb-cluster-us-east-1-yuln1.mongodb.net/test?retryWrites=true&w=majority',
+            'host': 'mongodb+srv://students:a428PmHV6DzfPMnk@mongodb-cluster-us-east-1-yuln1.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority',
         }
     }
 } 
