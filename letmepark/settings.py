96db25d9060e2e4adb116b2d1ee3e62fad12ce32
Steven de Salas <steven@desalasworks.com>
2020-04-20 17:54:03 +0200

    Moved static files to build/static to avoid directory recursion

--- letmepark/settings.py
+++ letmepark/settings.py
@@ -138,5 +138,5 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../build/static')
 STATICFILES_DIRS=(BASE_DIR, 'static')
