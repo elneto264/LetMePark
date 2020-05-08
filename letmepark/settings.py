750cad78f536dbbe924242b0cca2244b5bebc973
Ernesto <ernestoster@gmail.com>
2020-05-08 09:00:51 +0200

    Version con Google Maps
    Version con ajax funcional , pendiente de la lupa que funcione.

--- letmepark/settings.py
+++ letmepark/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','vaads)onh+h$6c8%fl&obuju!l!r&em
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['lmp.alwaysdata.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
